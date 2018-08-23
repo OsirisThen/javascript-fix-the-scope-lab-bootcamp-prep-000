e434d66c45c5ea170dd73014ddacff4fc8377834
Osiris Then <osiriscuevas@hotmail.com>
2018-08-23 19:38:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var animal = 'dog'
 
 function myAnimal() {
+  let animal=cat;
   return animal
 }
 
@@ -12,10 +13,11 @@ function yourAnimal() {
 }
 
 function add2(n) {
+  const two = 2
   return n + two
 
   // Feel free to move things around!
-  const two = 2
+  //const two = 2
 }
 
 var funkyFunction = function() {
