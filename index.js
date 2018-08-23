5f7d394b4bdd5e42140d48dcf61f0ee34a93e84a
Osiris Then <osiriscuevas@hotmail.com>
2018-08-23 19:46:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,10 +22,8 @@ function add2(n) {
 
 var funkyFunction = function() {
 
-  let p=function() {
     return "FUNKY!"
-  }
-  return p;
+
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
